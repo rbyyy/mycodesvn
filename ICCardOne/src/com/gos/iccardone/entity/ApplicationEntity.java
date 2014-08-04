7cf0db38e6df2c20398fc5d74b80b2c42dc3ac24
rbyyy <rbyyy924805@gmail.com>
2014-08-04 23:11:59 +0800

    Revert "addpro"
    This reverts commit 471ce7f6a013a51c6bfed0df044012e25df61acb.

--- ICCardOne/src/com/gos/iccardone/entity/ApplicationEntity.java
+++
@@ -1,5 +0,0 @@
-package com.gos.iccardone.entity;
-
-public interface ApplicationEntity {
-
-}
--- NZJCY/src/com/gov/nzjcy/util/DebugConfig.java
+++
@@ -1,8 +0,0 @@
-package com.gov.nzjcy.util;
-
-public final class DebugConfig {
-	
-	public final static boolean DEBUG = true;
-	public final static boolean USE_DEBUG_SERVER = true;
-	
-}
